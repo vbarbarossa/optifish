1c19ba152f700202c2a9bfde19798e61831f8c22
vbarbarossa <v.barbarossa@cml.leidenuniv.nl>
2022-02-21 23:34:14 +0100

    u

--- sh/calc_mekong_4var_lev12.sh
+++ sh/calc_mekong_4var_lev12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 3-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH -p "cpu-long"
 #SBATCH --output=sh/calc_mekong_4var_lev12.out
 #SBATCH --mail-type="ALL"
