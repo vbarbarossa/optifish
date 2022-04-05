9c7b7cbf4c7ca1d135fcfbbcb37eab8be5990c8e
vbarbarossa <v.barbarossa@cml.leidenuniv.nl>
2022-04-05 15:25:35 +0200

    up

---
+++ sh/optimize_caramel_lon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH -t 7-00:00:00
+#SBATCH -p "cpu-long"
+#SBATCH --output=sh/optimize_caramel_%a.out
+#SBATCH --mail-type=END
+#SBATCH --mail-user=v.barbarossa@cml.leidenuniv.nl
+
+module load Miniconda3
+source activate ~/envs/r4
+
+Rscript R/optimize_caramel.R
